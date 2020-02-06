38fce1f7cc0c8ed8f4fd99069bdf11094ded538e
gadamer1 <gadamer1@gadamer1ui-MacBookPro.local>
2020-02-07 02:01:33 +0900

    component test done

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,22 +1,37 @@
 import React, {Component} from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text, Button, StyleSheet} from 'react-native';
 
 class TodoItem extends Component {
-  constructor(props) {
-    super(props);
+  _onPressCompleteButton = () => {
+    const {onCompleted} = this.props;
+    onCompleted(this.props.index);
+  };
 
-    this.state = {};
-  }
+  _onPressDeleteButton = () => {
+    const {onDeleted} = this.props;
+    onDeleted(this.props.index);
+  };
 
   render() {
+    const {item} = this.props;
+
     return (
-      <View>
+      <View style={item.completed ? styles.completed : styles.default}>
         <Text></Text>
-        <Button title="add" />
-        <Button title="delete" />
+        <Button title="complete" onPress={this._onPressCompleteButton} />
+        <Button title="delete" onPress={this._onPressDeleteButton} />
       </View>
     );
   }
 }
+
+export const styles = StyleSheet.create({
+  default: {
+    backgroundColor: 'white',
+  },
+  completed: {
+    backgroundColor: 'gray',
+  },
+});
 
 export default TodoItem;
--- tests/TodoItem.test.js
+++ tests/TodoItem.test.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import TodoItem from '../src/components/TodoItem';
 import {shallow} from 'enzyme';
+import {styles} from '../src/components/TodoItem';
 
 describe('Rendering', () => {
   let wrapper;
+  let props;
 
   beforeEach(() => {
-    wrapper = shallow(<TodoItem />);
+    props = {
+      item: {
+        text: 'some Todo',
+        completed: false,
+      },
+    };
+    wrapper = shallow(<TodoItem {...props} />);
   });
 
   it('should render a text', () => {
@@ -15,5 +23,73 @@ describe('Rendering', () => {
 
   it('should render two Buttons', () => {
     expect(wrapper.find('Button')).toHaveLength(2);
+  });
+
+  describe('Uncompleted', () => {
+    it('should have default style', () => {
+      expect(wrapper.prop('style')).toBe(styles.default);
+    });
+  });
+
+  describe('Completed', () => {
+    beforeEach(() => {
+      props.item.completed = true;
+
+      wrapper = shallow(<TodoItem {...props} />);
+    });
+
+    it('should have completed style', () => {
+      expect(wrapper.prop('style')).toBe(styles.completed);
+    });
+  });
+});
+
+describe('Interaction', () => {
+  let wrapper;
+  let props;
+
+  describe('complete Feature', () => {
+    beforeEach(() => {
+      props = {
+        onCompleted: jest.fn(),
+        item: {
+          text: 'some Todo',
+          completed: false,
+        },
+        index: 0,
+      };
+      wrapper = shallow(<TodoItem {...props} />);
+      wrapper
+        .find('Button')
+        .at(0)
+        .prop('onPress')();
+    });
+    it('should call onCompleted with item index', () => {
+      expect(props.onCompleted).toHaveBeenCalledTimes(1);
+      expect(props.onCompleted).toHaveBeenCalledWith(props.index);
+    });
+  });
+
+  describe('Delete Feature', () => {
+    beforeEach(() => {
+      props = {
+        onDeleted: jest.fn(),
+        item: {
+          text: 'some Todo',
+          completed: true,
+        },
+        index: 0,
+      };
+      wrapper = shallow(<TodoItem {...props} />);
+      wrapper
+        .find('Button')
+        .at(1)
+        .prop('onPress')();
+    });
+
+    it('should call onDeleted with item index', () => {
+      expect(props.onDeleted).toHaveBeenCalledTimes(1);
+      expect(props.onDeleted).toHaveBeenCalledWith(props.index);
+    });
   });
 });
